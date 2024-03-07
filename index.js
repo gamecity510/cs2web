a713783a3716fbcea2009ed124e6c8403a58a750
sepanta pikani <153196701+gamecity510@users.noreply.github.com>
2024-03-07 20:10:26 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+function prime() {
+    document.body.style.background = "#00FFFF";
+}
+function blak() {
+    document.body.style.background = "#000000";
+}
+function white() {
+    document.body.style.background = " #DCDCDC";
+}
+function skin() {
+    document.getElementById('gifta').style.display = 'none';
+    document.getElementById('giftb').style.display = 'none';
+    document.getElementById('giftc').style.display = 'none';
+    document.getElementById('giftd').style.display = 'none';
+    document.getElementById('gifte').style.display = 'none';
+    document.getElementById('vido').style.display = '';
+    document.getElementById('vidoa').style.display = '';
+    document.getElementById('vidob').style.display = '';
+    document.getElementById('vidoc').style.display = '';
+    document.getElementById('vidod').style.display = '';
+    document.getElementById('vidoe').style.display = '';
+    document.getElementById('vidof').style.display = '';
+    document.getElementById('vidog').style.display = '';
+    document.getElementById('vidoh').style.display = '';
+    document.getElementById('vidoi').style.display = '';
+}
+   function  card() {
+document.getElementById('vido').style.display = 'none';
+document.getElementById('vidoa').style.display = 'none';
+document.getElementById('vidob').style.display = 'none';
+document.getElementById('vidoc').style.display = 'none';
+document.getElementById('vidod').style.display = 'none';
+document.getElementById('vidoe').style.display = 'none';
+document.getElementById('vidof').style.display = 'none';
+document.getElementById('vidog').style.display = 'none';
+document.getElementById('vidoh').style.display = 'none';
+document.getElementById('vidoi').style.display = 'none';
+document.getElementById('gifta').style.display = '';
+document.getElementById('giftb').style.display = '';
+document.getElementById('giftc').style.display = '';
+document.getElementById('giftd').style.display = '';
+document.getElementById('gifte').style.display = '';
+   }
+   function anpl(){
+    document.getElementById('vido').style.display = '';
+    document.getElementById('vidoa').style.display = '';
+    document.getElementById('vidob').style.display = '';
+    document.getElementById('vidoc').style.display = '';
+    document.getElementById('vidod').style.display = '';
+    document.getElementById('vidoe').style.display = '';
+    document.getElementById('vidof').style.display = '';
+    document.getElementById('vidog').style.display = '';
+    document.getElementById('vidoh').style.display = '';
+    document.getElementById('vidoi').style.display = '';
+    document.getElementById('gifta').style.display = '';
+    document.getElementById('giftb').style.display = '';
+    document.getElementById('giftc').style.display = '';
+    document.getElementById('giftd').style.display = '';
+    document.getElementById('gifte').style.display = '';
+   }
+//2
+const ghgh = document.getElementById('nifea');
+const gfrt = document.getElementById('addNumberButton');
+const hcxw = document.getElementById('ssga');
+const cgtt = document.getElementById('nifeb');
+const lojt = document.getElementById('pistola');
+const fdd = document.getElementById('counter');
+let yhg  = 0;
+
+
+ghgh.addEventListener('click', () => {
+  yhg++;
+ fdd.innerHTML=yhg*12000000;
+});
+gfrt.addEventListener("click", () => {
+yhg++
+fdd.innerHTML=yhg*30000;
+});
+hcxw.addEventListener("click", () => {
+    yhg++
+    fdd.innerHTML=yhg*50000;
+    });
+cgtt.addEventListener("click", () => {
+        yhg++
+        fdd.innerHTML=yhg*4000000;
+        });
+lojt.addEventListener("click", () => {
+            yhg++
+            fdd.innerHTML=yhg*100000;
+            });       
+      
+
+
